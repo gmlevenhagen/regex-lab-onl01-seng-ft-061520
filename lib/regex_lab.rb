47879a3bc495ddedab763745614fac8e8920708f
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-29 19:02:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,15 +1,13 @@
 def starts_with_a_vowel?(word)
 
-if word.match (/\A[aeiou]/i) == nil
-  
-  return false
-else
-  return true
+  if word.match (/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 
-
-end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
